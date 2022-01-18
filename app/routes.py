517701895ec41742ea18190dc44ee6c258d61eb8
Brian <bstanton773@gmail.com>
2022-01-18 11:50:24 -0600

    add template and context

--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,12 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 def index():
-    return 'Hello World!!!'
+    my_name = 'Brian'
+    my_city = 'Chicago'
+    colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet', 'black', 'magenta', 'pink']
+    return render_template('index.html', name=my_name, city=my_city, colors=colors)
 
 
 @app.route('/name')
@@ -14,4 +18,4 @@ def name():
 
 @app.route('/test')
 def test():
-    return '<h1>This is a test</h1>'
+    return '<h1>This is a test</h1><h4>This is an h4</h4>'
